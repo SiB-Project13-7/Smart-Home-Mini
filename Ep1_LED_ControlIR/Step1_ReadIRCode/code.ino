7ae8738fcd2cc40b3f0bb94dc301446533de0d19
SiB-Project13-7 <81.8.31.b@gmail.com>
2025-03-25 16:20:26 +0700

    Update code.ino (Step1_ReadIRCode)

---
+++ Ep1_LED_ControlIR/Step1_ReadIRCode/code.ino
@@ -0,0 +1,23 @@
+#include <IRremote.h>
+
+#define IR_RECEIVE_PIN 15  // Pin data sensor IR di ESP32
+
+void setup() {
+  Serial.begin(115200);  
+  IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);  // Aktifkan IR receiver
+  Serial.println("Siap terima sinyal remote!");
+}
+
+void loop() {
+  if (IrReceiver.decode()) {  // Cek ada sinyal masuk nggak
+    unsigned long kodeIR = IrReceiver.decodedIRData.decodedRawData;  // Ambil kode IR-nya
+
+    if (kodeIR != 0 && kodeIR != 0xFFFFFFFF) {  // Cek valid atau enggak
+      Serial.print("Kode IR Diterima: ");
+      Serial.println(kodeIR, HEX);  // Tampilkan kode dalam format HEX
+    }
+
+    IrReceiver.resume();  // Reset untuk baca sinyal berikutnya
+  }
+  delay(100);
+}
